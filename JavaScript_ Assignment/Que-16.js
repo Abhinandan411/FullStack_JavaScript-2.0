1b480d62cbf96bfa92e8614b47182afa7f25ba57
Abhinandan Kumar <abhinandankumar6204@gmail.com>
2023-07-24 22:12:45 +0530

    Added Qu-16 of JS assignment

---
+++ JavaScript_ Assignment/Que-16.js
@@ -0,0 +1,40 @@
+// Question:16-------
+
+// 16. Check if the season is Autumn, Winter, Spring or Summer. If the user input is :
+//     - September, October or November, the season is Autumn.
+//     - December, January or February, the season is Winter.
+//     - March, April or May, the season is Spring
+//     - June, July or August, the season is Summer
+
+
+// Solution:--
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+const month = prompt("Enter the month: ").toLowerCase();
+
+switch (month) {
+  case "september":
+  case "october":
+  case "november":
+    console.log('This is Autumn season');
+    break;
+  case "december":
+  case "january":
+  case "february":
+    console.log('This is Winter season');
+    break;
+  case "march":
+  case "april":
+  case "may":
+    console.log('This is Spring season');
+    break;
+  case "june":
+  case "july":
+  case "august":
+    console.log('This is Summer season');
+    break;
+  default:
+    console.log('Please Enter a correct month');
+    break;
+}

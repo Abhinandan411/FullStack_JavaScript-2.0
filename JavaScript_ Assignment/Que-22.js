ccb0d3c2316ba7a25ed6d0f496e494fc3c25c0d2
Abhinandan Kumar <abhinandankumar6204@gmail.com>
2023-07-30 09:46:15 +0530

    Added Que-23 of JS Assignment

--- JavaScript_ Assignment/Que-22.js
+++ JavaScript_ Assignment/Que-22.js
@@ -1,3 +1,5 @@
+// Question:22-------
+
 // 22. The following is an array of 10 students ages:
 //     => const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
 //     - Sort the array and find the min and max age
@@ -5,6 +7,10 @@
 //     - Find the average age(all items divided by number of items)
 //     - Find the range of the ages(max minus min)
 //     - Compare the value of (min - average) and (max - average), use abs() method
+
+
+
+// Solution:--
 
 const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24];
 

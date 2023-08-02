4cce072817884ba1fbbb235587c61bbad95d6928
Abhinandan Kumar <abhinandankumar6204@gmail.com>
2023-08-02 09:53:28 +0530

    Added Que-25 of JS assignment

--- JavaScript_ Assignment/Que-25.js
+++ JavaScript_ Assignment/Que-25.js
@@ -1,4 +1,4 @@
-// Question:24-------
+// Question:25-------
 
 // 25. Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
 //     - The same groups apply to both men and women.

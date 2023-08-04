c61ee99d6ef15cbbea0461775f7a8b8ed6873253
Abhinandan Kumar <abhinandankumar6204@gmail.com>
2023-08-04 18:29:08 +0530

    Added Que-27 of JS assignment

--- JavaScript_ Assignment/Que-27.js
+++ JavaScript_ Assignment/Que-27.js
@@ -2,6 +2,7 @@
 
 // 27. Write a program to print all the prime number between 0 to 100 (0 and 100 included).
 
+// Solution:--
 function primeNumber(num) {
     if (num == 0 || num == 1) {
       return `${num} is not a prime number`;

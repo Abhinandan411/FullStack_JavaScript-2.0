5e2a251f5374fd98a834329496fb10b5a4fda92c
Abhinandan Kumar <abhinandankumar6204@gmail.com>
2023-08-08 23:36:07 +0530

    Added Que-29 of JS assignment

--- JavaScript_ Assignment/Que-29.js
+++ JavaScript_ Assignment/Que-29.js
@@ -1,6 +1,8 @@
 // Question:29------
 // 29. Write a javascript program which takes the input of filename as string and prints the extension of the file in the console.
 
+
+// Solution:--
 const prompt = require("prompt-sync")({ sigint: true });
 
 const printExtension = () => {
